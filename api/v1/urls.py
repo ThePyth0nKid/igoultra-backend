6cdc50a28a2d52c18fb4b08991c35c070e17cc34
Nelson <nelson.mehlis@icloud.com>
2025-04-14 16:35:52 +0200

    Add initial URL routing for authentication, user, XP, and seasons modules

---
+++ api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("auth/", include("api.v1.auth.urls")),
+    path("user/", include("api.v1.user.urls")),
+    path("xp/", include("api.v1.xp.urls")),
+    path("seasons/", include("api.v1.seasons.urls")),
+]
