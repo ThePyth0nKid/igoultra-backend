2714b4f77edc73c4513f36c737fc4fa71138c454
Nelson <nelson.mehlis@icloud.com>
2025-05-08 18:46:43 +0200

    Refactor urls.py: add CSRF endpoint to enhance session management

--- api/v1/auth/urls.py
+++ api/v1/auth/urls.py
@@ -4,6 +4,7 @@ from users.views import MeView
 urlpatterns = [
     # DJ Rest Auth endpoints for session login/logout
     path('', include('dj_rest_auth.urls')),
+    path("csrf/", include("dj_rest_auth.urls")),
     # Registration endpoints (including social login)
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('me/', MeView.as_view(), name="custom_me_view"),
